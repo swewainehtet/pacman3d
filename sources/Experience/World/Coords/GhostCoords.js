5c0692eeef5692b5cce4f67c0612c45daad22451
swewainehtet <swewainehtet@gmail.com>
2023-03-08 17:57:17 -0800

    file restructure

---
+++ sources/Experience/World/Coords/GhostCoords.js
@@ -0,0 +1,6 @@
+export const ghostCoords = [
+  { x: 12.5, y: 19, color: 0xff0000 },
+  { x: 13.5, y: 19, color: 0x00ff00 },
+  { x: 14.5, y: 19, color: 0x0000ff },
+  { x: 15.5, y: 19, color: 0xffff00 },
+];
